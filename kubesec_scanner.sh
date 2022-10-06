93baca819da2d48da3f2ef6b988ab5c4f5356ec3
unknown <okansungur77@gmail.com>
2022-10-06 09:12:48 +0300

    Kubesec added..

--- kubesec_scanner.sh
+++ kubesec_scanner.sh
@@ -3,9 +3,9 @@
 #kubesec-scan.sh
 
 # using kubesec v2 api
-scan_result=$(curl -sSX POST --data-binary @"${deploymentfilename}" https://v2.kubesec.io/scan)
-scan_message=$(curl -sSX POST --data-binary @"${deploymentfilename}" https://v2.kubesec.io/scan | jq .[0].message -r ) 
-scan_score=$(curl -sSX POST --data-binary @"${deploymentfilename}" https://v2.kubesec.io/scan | jq .[0].score ) 
+scan_result=$(curl -sSX POST --data-binary @${deploymentfilename} https://v2.kubesec.io/scan)
+scan_message=$(curl -sSX POST --data-binary @${deploymentfilename} https://v2.kubesec.io/scan | jq .[0].message -r ) 
+scan_score=$(curl -sSX POST --data-binary @${deploymentfilename} https://v2.kubesec.io/scan | jq .[0].score ) 
 
 	
     # Kubesec scanning result 
