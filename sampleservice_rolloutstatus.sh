11836aa2d343863d773ca5a10136e843ce62a5f3
unknown <okansungur77@gmail.com>
2022-10-06 08:20:52 +0300

    rollout history added

--- sampleservice_rolloutstatus.sh
+++ sampleservice_rolloutstatus.sh
@@ -3,10 +3,10 @@
 
 sleep 40s
 
-if [[ $(kubectl -n default rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]]; 
+if [[ $(kubectl -n ${namespace} rollout status deploy ${deploymentName} --timeout 5s) != *"successfully rolled out"* ]]; 
 then     
 	echo "Deployment ${deploymentName} Rollout has Failed"
-    kubectl -n default rollout undo deploy ${deploymentName}
+    kubectl -n ${namespace} rollout undo deploy ${deploymentName}
     exit 1;
 else
 	echo "Deployment ${deploymentName} Rollout is Success"
