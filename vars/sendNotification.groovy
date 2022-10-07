6d90a888637449e9aa60cac10a503590588c1110
unknown <okansungur77@gmail.com>
2022-10-07 14:18:36 +0300

    Slack added.

---
+++ vars/sendNotification.groovy
@@ -0,0 +1,21 @@
+def call(String buildStatus = 'STARTED') {
+ buildStatus = buildStatus ?: 'SUCCESS'
+
+ def color
+
+ if (buildStatus == 'SUCCESS') {
+  color = '#47ec05'
+  emoji = ':ww:'
+ } else if (buildStatus == 'UNSTABLE') {
+  color = '#d5ee0d'
+  emoji = ':deadpool:'
+ } else {
+  color = '#ec2805'
+  emoji = ':hulk:'
+ }
+
+ def msg = "${buildStatus}: `${env.JOB_NAME}` #${env.BUILD_NUMBER}:\n${env.BUILD_URL}"
+
+ slackSend(color: color, message: msg)
+
+}
