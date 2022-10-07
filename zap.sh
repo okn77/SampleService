37240da8a04a66f674e8f13174c9ff428949068a
unknown <okansungur77@gmail.com>
2022-10-07 13:17:58 +0300

    ZAP added

---
+++ zap.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+PORT=$(kubectl -n prod get svc ${serviceName} -o json | jq .spec.ports[].nodePort)
+#31932 port
+
+chmod 777 $(pwd)
+echo $(id -u):$(id -g)
+docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -r zap_report.html
+
+
+# comment above cmd and uncomment below lines to run with CUSTOM RULES
+#docker run -v $(pwd):/zap/wrk/:rw -t owasp/zap2docker-weekly zap-api-scan.py -t $applicationURL:$PORT/v3/api-docs -f openapi -c zap_rules -r zap_report.html
+
+exit_code=$?
+
+
+# HTML Report
+ sudo mkdir -p owasp-zap-report
+ sudo mv zap_report.html owasp-zap-report
+
+
+echo "Exit Code : $exit_code"
+
+ if [[ ${exit_code} -ne 0 ]];  then
+    echo "OWASP ZAP Report has either Low/Medium/High Risk. Please check the HTML Report"
+    exit 1;
+   else
+    echo "OWASP ZAP did not report any Risk"
+ fi;
+
+
